a47bb9ae2dda1d5a9e93f8c4d95dd777ffaba0db
borhanhassan1 <166884594+borhanhassan1@users.noreply.github.com>
2025-08-07 13:01:29 +0300

    Update stripeRoutes.js

--- server/routes/stripeRoutes.js
+++ server/routes/stripeRoutes.js
@@ -11,7 +11,6 @@ const stripeRouter = express.Router();
 stripeRouter.post("/checkout", userAuth, stripeCheckout);
 stripeRouter.post(
   "/webhook",
-  bodyParser.raw({ type: "application/json" }),
   stripeWebHook
 );
 export default stripeRouter;
